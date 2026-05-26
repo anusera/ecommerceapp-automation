f44c69e6ff9688c6b7f1b604857a1fb76f644330
anusera <84999479+anusera@users.noreply.github.com>
2026-05-26 13:55:23 +0530

    Added Base class

---
+++ EcommerceAppAutomation/src/test/java/automationcore/Base.java
@@ -0,0 +1,33 @@
+package automationcore;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+	
+		    public WebDriver driver;
+
+	    @BeforeMethod
+	    public void setup() {
+
+	        WebDriverManager.chromedriver().setup();
+
+	        driver = new ChromeDriver();
+
+	        driver.manage().window().maximize();
+
+	        driver.get("https://example.com");
+	    }
+
+	    @AfterMethod
+	    public void tearDown() {
+
+	        driver.quit();
+	    }
+
+
+}
