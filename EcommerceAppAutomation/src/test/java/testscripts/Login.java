f654463629dbd996b349633873f1a6ceb8041d6a
anusera <84999479+anusera@users.noreply.github.com>
2026-05-29 11:34:09 +0530

    Changes loginTest to loginTestAdmin

--- EcommerceAppAutomation/src/test/java/testscripts/Login.java
+++ EcommerceAppAutomation/src/test/java/testscripts/Login.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class Login {
 	
 	@Test
-	public void loginTest() {
+	public void loginTestAdmin() {
 		System.out.println("Executing login test...");
 		// Add your login test logic here
 	}
