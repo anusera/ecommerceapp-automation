022ce816667e57f4b24f2b8bbe00e9ddc155398f
anusera <84999479+anusera@users.noreply.github.com>
2026-05-29 12:43:45 +0530

    c

--- EcommerceAppAutomation/src/main/java/pages/LoginPage.java
+++ EcommerceAppAutomation/src/main/java/pages/LoginPage.java
@@ -1,5 +1,9 @@
 package pages;
 
 public class LoginPage {
+	public void login(String username, String password) {
+		// Code to perform login action using the provided username and password
+		System.out.println("Logging in with username: " + username + " and password: " + password);
+	}
 
 }
